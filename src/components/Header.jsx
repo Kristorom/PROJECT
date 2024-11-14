710abf1c3041e10fc3a674586b3102407b2d3ef9
Kristorom <khrystyna.stad@gmail.com>
2024-11-14 08:29:41 +0200

    Added adaptive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Header.css';
-import '../styles/Home.css';
-
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className="header">
             <nav className="nav-bar">
                 <div className="content">
-                    <ul className="navigation">
+                    <div className="mobile-logo-and-menu">
+                        <div className="site-logo"><Link to="/">DreamTrip</Link></div>
+                        <div className="burger-menu" onClick={toggleMenu}>
+                            &#9776;
+                        </div>
+                    </div>
+                    <ul className={`navigation ${isOpen ? 'open' : ''}`}>
                         <li className="link"><Link to="/">Main</Link></li>
                         <li className="link"><Link to="/services">Services</Link></li>
                         <li className="link"><Link to="/about">About</Link></li>
-                        <li className="site-logo"><Link to="/">DreamTrip</Link></li>
+                        <li className="site-logo desktop-only"><Link to="/">DreamTrip</Link></li>
                         <li className="link"><Link to="/tours">Tours</Link></li>
                         <li className="link"><Link to="/contacts">Contacts</Link></li>
                         <li className="link"><Link to="/login">Log In</Link></li>
@@ -21,7 +31,6 @@ const Header = () => {
                 </div>
             </nav>
         </header>
-      
     );
 };
 
